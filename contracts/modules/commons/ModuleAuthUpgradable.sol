3cffa83b9ab7ddde11bb041ca653d6330ded062c
Shun Kakinoki <shunkakinoki@gmail.com>
2024-10-14 18:19:53 +0900

    chore: ini imageHashes validation

--- contracts/modules/commons/ModuleAuthUpgradable.sol
+++ contracts/modules/commons/ModuleAuthUpgradable.sol
@@ -9,21 +9,23 @@ import "./ModuleStorage.sol";
 
 
 abstract contract ModuleAuthUpgradable is IModuleAuthUpgradable, ModuleSelfAuth, ModuleAuth {
+  mapping(address => bytes32) public imageHashes;
   /**
    * @notice Updates the signers configuration of the wallet
    * @param _imageHash New required image hash of the signature
    */
-  function _updateImageHash(bytes32 _imageHash) internal override virtual {
+  function _updateImageHash(address _account, bytes32 _imageHash) internal override virtual {
     if (_imageHash == bytes32(0)) revert ImageHashIsZero();
-    ModuleStorage.writeBytes32(IMAGE_HASH_KEY, _imageHash);
-    emit ImageHashUpdated(_imageHash);
+    imageHashes[_account] = _imageHash;
+    // ModuleStorage.writeBytes32(IMAGE_HASH_KEY, _imageHash);
+    emit ImageHashUpdated(_account, _imageHash);
   }
 
   /**
    * @notice Returns the current image hash of the wallet
    */
-  function imageHash() external override virtual view returns (bytes32) {
-    return ModuleStorage.readBytes32(IMAGE_HASH_KEY);
+  function imageHash(address _account) external override virtual view returns (bytes32) {
+    return imageHashes[_account];
   }
 
   /**
@@ -32,8 +34,8 @@ abstract contract ModuleAuthUpgradable is IModuleAuthUpgradable, ModuleSelfAuth,
    * @param _imageHash Hash image of signature
    * @return true if the signature image is valid
    */
-  function _isValidImage(bytes32 _imageHash) internal override virtual view returns (bool) {
-    return _imageHash != bytes32(0) && _imageHash == ModuleStorage.readBytes32(IMAGE_HASH_KEY);
+  function _isValidImage(address _account, bytes32 _imageHash) internal override virtual view returns (bool) {
+    return _imageHash != bytes32(0) && _imageHash == imageHashes[_account];
   }
 
   /**
--- contracts/modules/commons/interfaces/IModuleAuth.sol
+++ contracts/modules/commons/interfaces/IModuleAuth.sol
@@ -6,13 +6,14 @@ abstract contract IModuleAuth {
   //                        IMAGE_HASH_KEY = keccak256("org.arcadeum.module.auth.upgradable.image.hash");
   bytes32 internal constant IMAGE_HASH_KEY = bytes32(0xea7157fa25e3aa17d0ae2d5280fa4e24d421c61842aa85e45194e1145aa72bf8);
 
-  event ImageHashUpdated(bytes32 newImageHash);
+  event ImageHashUpdated(address account, bytes32 newImageHash);
 
   // Errors
   error ImageHashIsZero();
   error InvalidSignatureType(bytes1 _type);
 
   function _signatureValidation(
+    address _account,
     bytes32 _digest,
     bytes calldata _signature
   ) internal virtual view returns (
@@ -35,7 +36,7 @@ abstract contract IModuleAuth {
    * @notice Validates the signature image
    * @return true if the signature image is valid
    */
-  function _isValidImage(bytes32) internal virtual view returns (bool) {
+  function _isValidImage(address,bytes32) internal virtual view returns (bool) {
     return false;
   }
 
@@ -43,11 +44,11 @@ abstract contract IModuleAuth {
    * @notice Updates the signers configuration of the wallet
    * @param _imageHash New required image hash of the signature
    */
-  function updateImageHash(bytes32 _imageHash) external virtual;
+  function updateImageHash(address _account, bytes32 _imageHash) external virtual;
 
   /**
    * @notice Updates the signers configuration of the wallet
    * @param _imageHash New required image hash of the signature
    */
-  function _updateImageHash(bytes32 _imageHash) internal virtual;
+  function _updateImageHash(address _account, bytes32 _imageHash) internal virtual;
 }
